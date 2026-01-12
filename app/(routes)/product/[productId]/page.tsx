db07638eab793d9aad61b87de022e9e22b5edb76
King-Lota <lottanna47@gmail.com>
2026-01-12 13:01:37 +0100

    Add Product page component: Implement product detail page with metadata generation and integrate ProductDetailPage component for displaying product information.

---
+++ app/(routes)/product/[productId]/page.tsx
@@ -0,0 +1,37 @@
+import ProductDetailPage from "@/components/sections/product.detail";
+import { type Metadata } from "next";
+
+
+export async function generateMetadata({ params }: { params: { productId: string } }): Promise<Metadata> {
+
+    const getPlants = ''
+
+
+    if (!getPlants)
+        return {
+            title: "The Garden Fairy",
+            description: "Discover beautiful indoor plants to enhance your space. Browse our collection of vibrant, easy-to-care-for plants perfect for every home."
+        };
+
+    return {
+        title: "The Garden Fairy",
+        description: "Discover beautiful indoor plants to enhance your space. Browse our collection of vibrant, easy-to-care-for plants perfect for every home."
+
+    }
+
+
+}
+
+
+const ProductPage = ({ params }: {
+    params: {
+        productId: string
+    }
+}) => {
+
+    return (
+        <ProductDetailPage />
+    )
+}
+
+export default ProductPage
